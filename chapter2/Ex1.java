8d7c79be8aca683a420a8824071c9c9f08409821
WiseMordecai <osamalearningjp@gmail.com>
2023-01-08 10:27:12 +0300

    create chapter2 and Ex1

---
+++ chapter2/Ex1.java
@@ -0,0 +1,8 @@
+package com.advancedoop.theory.chapter2;
+
+public class Ex1 {
+  public static void main(String[] args) {
+    Thread thread = Thread.currentThread();
+    System.out.println("Thread : " + thread);
+  }
+}
